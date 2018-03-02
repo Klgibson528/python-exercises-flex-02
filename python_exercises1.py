618903116dd70af8de805de47d8c9cba07db8271
Katherine Gibson <klgibson528@gmail.com>
2018-03-02 19:09:50 +0000

    + ex 2

--- python_exercises1.py
+++ python_exercises1.py
@@ -1,8 +1,9 @@
-# #Exercise 1 - Hello, you!
-# name = raw_input("What is your name? ")
-# print "Hello, "+ name + "!"
+#Exercise 1 - Hello, you!
+name = raw_input("What is your name? ")
+print "Hello, " + name + "!"
   
 #Exercise 2 - HELLO, YOU!
 name = raw_input("What is your name? ")
 new_string = "Hello, "+ name + "!"
-print new_string.upper
+print new_string.upper()
+
