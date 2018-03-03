39d55c4c36c992a134920ee900798c3b99c7adb0
Katherine Gibson <klgibson528@gmail.com>
2018-03-03 03:19:07 +0000

    ex 1-7 and 9

--- python_exercises1.py
+++ python_exercises1.py
@@ -13,21 +13,63 @@
 # food= raw_input("Give me a food ")
 # print "%s is a %s who enjoys eating %s" % (name, job, food)
 
-# Exercise 4 - Day of the Week
-day = int(input('Day (0-6)? '))
-if day == 0:
-  print "Sunday"
-elif day == 1:
-  print "Monday"
-elif day == 2:
-  print "Tuesday"
-elif day == 3:
-  print "Wednesday"
-elif day == 4:
-  print "Thursday"
-elif day == 5:
-  print "Friday"
-elif day == 6:
-  print "Saturday"
-else:
-  print "That number is not between 0 and 6"
+# #Exercise 4 - Day of the Week
+# day = int(input('Day (0-6)? '))
+# if day == 0:
+#   print "Sunday"
+# elif day == 1:
+#   print "Monday"
+# elif day == 2:
+#   print "Tuesday"
+# elif day == 3:
+#   print "Wednesday"
+# elif day == 4:
+#   print "Thursday"
+# elif day == 5:
+#   print "Friday"
+# elif day == 6:
+#   print "Saturday"
+# else:
+#   print "That number is not between 0 and 6"
+
+# #Exercise 5 - Work or Sleep In?
+# day = int(input('Day (0-6)? '))
+# if day in range(1, 5):
+#   print "Go to work!"
+# elif day == 0 or day == 6:
+#   print "You get to sleep in!"
+# else:
+#   print "That number is not between 0 and 6"
+
+# #Exercise 6 - Celsius to Fahrenheit
+# cel = int(input("Temperature in Celsius? "))
+# far = cel * 1.8 + 32
+# print str(cel) + " degrees celsius is equal to "+ str(far) +" degrees fahrenheit."
+
+# #Exercise 7 - Tip Calculator
+# total = int(input("Total bill amount? "))
+# service = raw_input("Was the service good, fair, or bad? ")
+# service = service.lower()
+# if service == "good":
+#   tip = .20 * total
+#   print "Since your service was good you should leave a 20% tip which would be " + "$" + str("%.2f"%tip)+"."
+# elif service == "fair":
+#   tip = .15 * total
+#   print "Since your service was fair you should leave a 15% tip which would be " + "$" + str("%.2f"%tip)+"."
+# elif service == "bad":
+#   tip = .10 * total
+#   print "Since your service was bad you should leave a 10% tip which would be " + "$" + str("%.2f"%tip)+"."
+# else:
+#   print "Please rate your service as 'Good', 'Fair' or 'Bad'"
+# #I want to figure out how to make it loop back if they enter something other than good, bad, or fair
+
+#Exercise 8 - Tip Calculator 2
+
+
+# #Exercise 9 - 1 to 10
+# count = 0
+# while count < 10:
+#   count += 1
+#   print count
+
+#Exercise 10 - How many coins?
