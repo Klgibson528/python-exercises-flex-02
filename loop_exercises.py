b16f4562df26305dd317f421c41b123a29e48749
Katherine Gibson <klgibson528@gmail.com>
2018-03-05 20:38:39 +0000

    ex 1-5

---
+++ loop_exercises.py
@@ -0,0 +1,32 @@
+# # 1 to 10
+# count = 0 
+# while count in range(0,10):
+#     count += 1
+#     print count
+
+# # n to m
+# n = int(input("Where would you like to start? "))
+# m = int(input("Where would you like to end? "))
+# count = n-1
+# while count in range(n-1, m):
+#     count += 1
+#     print count
+
+# # Odd numbers
+# count = 1 
+# while count in range(1, 10):
+#     print count
+#     count += 2 
+    
+# # 5 x 5 Square
+# count = 0 
+# while count < 5:
+#     print "*" * 5
+#     count += 1
+
+# How big is the square?
+count = 0
+big = int(input("How big is the square? "))
+while count < big:
+    print "*" * big
+    count += 1
