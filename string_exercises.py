65ebe0def849f8458cdffbf83cd4029c4978b79b
Katherine Gibson <klgibson528@gmail.com>
2018-03-06 22:34:05 +0000

    finished through 4

--- string_exercises.py
+++ string_exercises.py
@@ -10,3 +10,41 @@
 # for i in word:
 #     string = i + string
 # print string 
+
+# #Leet speak
+# word = raw_input("What word would you like to convert to Leet speak? ")
+# word = word.upper()
+# leet = ""
+# for i in word:
+#   if i == "A":
+#       i = "4"
+#       leet = leet + i
+#   elif i == "E":
+#       i = "3"
+#       leet = leet + i
+#   elif i == "G":
+#       i = "6"
+#       leet = leet + i
+#   elif i == "I":
+#       i = "1"
+#       leet = leet + i
+#   elif i == "O":
+#       i = "0"
+#       leet = leet + i 
+#   elif i == "S":
+#       i = "5"
+#       leet = leet + i
+#   elif i == "T":
+#       i = "7"
+#       leet = leet + i    
+#   else:
+#       leet = leet + i
+    
+# print leet
+# # Could you use a dictionary for this?
+
+# Long-long vowels
+word = raw_input("Give me a word ")
+long_word=""
+for i in word:
+    
