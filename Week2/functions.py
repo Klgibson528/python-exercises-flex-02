1fce03be2f35abdafd7ac9ada3819dec8b89a341
Katherine Gibson <klgibson528@gmail.com>
2018-03-09 00:34:48 +0000

    working on mathplotlib

--- Week2/functions.py
+++ Week2/functions.py
@@ -1,1 +1,72 @@
-# Functions
+# # Functions
+# def hello(name):
+#     print ("Hello {}".format(name))
+# hello(input("What is your name? "))
+
+# # y = x + 1
+# import matplotlib
+# matplotlib.use("Agg")
+
+# from matplotlib import pyplot
+
+# def f(x):
+#     return x+1
+
+# xs = list(range(-3, 4))
+# ys = []
+# for x in xs:
+#     ys.append(f(x))
+
+# pyplot.plot(xs, ys)
+# pyplot.savefig('Number1.png')
+# pyplot.close()
+
+# # y = x^2
+# import matplotlib
+# matplotlib.use("Agg")
+
+# from matplotlib import pyplot
+
+# def f(x):
+#     return x*x
+
+# xs = list(range(-100, 101))
+# ys = []
+# for x in xs:
+#     ys.append(f(x))
+
+# pyplot.plot(xs, ys)
+# pyplot.savefig('Number2.png')
+# pyplot.close()
+
+# # Odd or Even
+# import matplotlib
+# matplotlib.use("Agg")
+
+# from matplotlib import pyplot
+
+# def f(x):
+#     if x % 2 == 0:
+#         return -1
+#     else:
+#         return 1
+
+# xs = list(range(-5, 6))
+# ys = []
+# for x in xs:
+#     ys.append(f(x))
+
+# pyplot.bar(xs, ys)
+# pyplot.savefig('Number3.png')
+# pyplot.close()
+
+# def play():
+#     ans = input("Would you like to play again? Y or N ")
+#     ans = ans.upper()
+#     if ans == "Y":
+#         return (True)
+#     else:
+#         return (False) 
+# play()
+
+
