fce8bb436da3f68148503ab32ee04771724808e4
Katherine Gibson <klgibson528@gmail.com>
2018-03-11 22:53:27 +0000

    finished phonebook

--- Week2/phonebookapp.py
+++ Week2/phonebookapp.py
@@ -1,57 +1,66 @@
 # Phonebook app
-book = [{'name':'Katy Gibson'  'phonenumber':'281-728-7870' 'email': 'klgibson528@gmail.com'}]
-
-def intro():
-    print ("""Electronic Phone Book
-=====================
-1. Look up an entry
-2. Set an entry
-3. Delete an entry
-4. List all entries
-5. Save entries
-6. Restore saved entries
-7. Quit""")
-#use pickle to save data 
-#use pickle to call saved data
-    ans = int(input("What would you like to do? Please enter a number. "))
-
-    if ans == 1:
-        ent = input("Who would you like to look up? ")
-        print (book[ent])  
-        
-    elif ans == 2:
-        name = input("Full name: ")
-        num = input("Phone number: ")
-        email = input("Email: ")
-        website = input("Website: ")
-        book[name] = num
-        print ('An entry for ' + name + " was saved.")
-        
-    elif ans == 3:
-        name = input("Who would you like to delete? ")
-        del book[name]
-        
-    
-    elif ans == 4:
-        for key, value in book.items():
-            print ('{}: {}'.format(key, value))
-    elif ans == 5:
-        
-    elif ans == 6:
-        
-    else:
-        print ("Thank you for using the phonebook!")
-
-def save_entries:
-    import pickle
-    with open('data.pickle', 'wb') as fh:
-        pickle.dump(book, fh)
-
-def load_entries:
-    import pickle
+import pickle
+def load_entries():
+    global book
     with open('data.pickle', 'rb') as fh:
         book = pickle.load(fh)
-        print(book)
-    
-intro()
+load_entries()
 
+def save_entries():
+    with open('data.pickle', 'wb') as fh:
+        pickle.dump(book, fh)
+        
+book = {}
+
+def intro():
+    while True:
+        print ("""Electronic Phone Book)
+    =====================
+    1. Look up an entry
+    2. Set an entry
+    3. Delete an entry
+    4. List all entries
+    5. Save entries
+    6. Restore saved entries
+    7. Quit""")
+    #use pickle to save data 
+    #use pickle to call saved data
+        ans = int(input("What would you like to do? Please enter a number. "))
+        if ans == 1:
+            ent = input("Who would you like to look up? ")
+            ent = ent.lower()
+            load_entries()
+            if ent in book:
+                print (book[ent])
+            else:
+                print ("There is no listing for that person.")
+        elif ans == 2:
+            name = input("Full name: ")
+            name = name.lower()
+            num = input("Phone number: ")
+            email = input("Email: ")
+            book[name] = {}
+            book[name]['phone'] = num
+            book[name]['email'] = email
+            save_entries()
+            print ('An entry for ' + name + " was saved.")
+            
+        elif ans == 3:
+            name = input("Who would you like to delete? ")
+            name = name.lower()
+            load_entries()
+            del book[name]
+            save_entries()
+        elif ans == 4:
+            load_entries()
+            for key, value in book.items():
+                print ('{}: {}'.format(key, value))
+        elif ans == 5:
+            save_entries()
+        elif ans == 6:
+            load_entries()
+        else:
+            print ("Thank you for using the phonebook!")
+            break
+
+intro()7
