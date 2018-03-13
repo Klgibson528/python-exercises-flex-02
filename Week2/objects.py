46b3057c3601490d9322349d91c5588b60ef4017
Katherine Gibson <klgibson528@gmail.com>
2018-03-13 21:12:16 +0000

    finished!

--- Week2/objects.py
+++ Week2/objects.py
@@ -1,15 +1,18 @@
 # # Basics
 # class Person:
-#     def __init__(self, name, email, phone):
+#     def __init__(self, name, email, phone, greet_count):
 #         self.name = name
 #         self.email = email
 #         self.phone = phone
+#         self.greet_count = greet_count
 #         friends = []
 #         self.friends = friends
 
 #     def greet(self, other_person):
 #         print('Hello {}, I am {}!'.format(other_person.name, self.name))
-   
+#         self.greet_count += 1
+#         print('You have had {} greetings!'.format(self.greet_count))
+        
 #     def contact(self):
 #         return ('Name: {}\nEmail: {}\nPhone: {}').format(self.name, self.email, self.phone)
     
@@ -21,9 +24,10 @@
 #     def __str__(self):
 #         return 'Name: {}\nEmail: {}\nPhone: {}'.format(self.name, self.email, self.phone)
 
-# sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
-# jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
+# sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948', 0)
+# jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456', 0)
 
+# sonny.greet(jordan)
 # sonny.greet(jordan)
 # jordan.greet(sonny)
 
